f5086135b079486d3c7ab9ceba0cd173afcea8de
Bhrikuty Aggarwal <bhrikuty.aggarwal@gmail.com>
2021-03-26 14:41:48 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.This is a CICD test</p>
+                <p>Your PHP application is now running on a container in Amazon ECS.This is where buildspec file is coming from the template itself</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
